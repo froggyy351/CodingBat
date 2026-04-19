2a6a37fc1cd33b972233d23d9d42f515b989527a
Koichi Sakogawa <kouichi19980817@gmail.com>
2026-04-19 15:52:01 +0900

    answered close10

---
+++ Warmup-1/close10.java
@@ -0,0 +1,13 @@
+public int close10(int a, int b) {
+  int absA = Math.abs(10 - a);
+  int absB = Math.abs(10 - b);
+  
+  if(absA == absB){
+    return 0;
+  }else if(absA > absB){
+    return b;
+  }else{
+    // absA < absB
+    return a;
+  }
+}
